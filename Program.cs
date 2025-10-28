4b534e6597f35fb9033b80289c29bc541a80515d
Saido <saidoteknoloji@gmail.com>
2025-10-28 20:41:17 +0300

    İlk kayıt sistemi eklendi

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Adınızı girin :");
+        string ad = Console.ReadLine();
+
+        Console.WriteLine("Soyadınızı girin :");
+        string soyad = Console.ReadLine();
+
+        Console.WriteLine("Unvanınızı Yazın :");
+        string unvan = Console.ReadLine();
+
+        Console.WriteLine("Mesleğinizi girin :");
+        string meslek = Console.ReadLine();
+
+        Console.WriteLine("ADI" + ad);
+        Console.WriteLine("SOY ADI" + soyad);
+        Console.WriteLine("UNVAN" + unvan);
+        Console.WriteLine("MESLEK" + meslek);
+    }
+    }
